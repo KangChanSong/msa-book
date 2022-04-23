0e19d87ad6b0de6001184775c63561ad7c27c2ea
송강찬 <76604142+KangChanSong@users.noreply.github.com>
2022-04-23 20:52:12 +0900

    Dockerfile 생성

---
+++ microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/services/ProductCompositeIntegration.java
@@ -0,0 +1,129 @@
+package se.magnus.microservices.composite.product.services;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+import se.magnus.api.core.product.Product;
+import se.magnus.api.core.product.ProductService;
+import se.magnus.api.core.recommendation.Recommendation;
+import se.magnus.api.core.recommendation.RecommendationService;
+import se.magnus.api.core.review.Review;
+import se.magnus.api.core.review.ReviewService;
+import se.magnus.util.exceptions.InvalidInputException;
+import se.magnus.util.exceptions.NotFoundException;
+import se.magnus.util.http.HttpErrorInfo;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class ProductCompositeIntegration implements ProductService, RecommendationService, ReviewService {
+
+    private final Logger LOG = LoggerFactory.getLogger(ProductCompositeIntegration.class);
+
+    private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
+    private final String productServiceUrl;
+    private final String recommendationServiceUrl;
+    private final String reviewServiceUrl;
+
+    @Autowired
+    public ProductCompositeIntegration(
+            RestTemplate restTemplate,
+            ObjectMapper objectMapper,
+
+            @Value("${app.product-service.host}") String productServiceHost,
+            @Value("${app.product-service.port}") int productServicePort,
+            @Value("${app.recommendation-service.host}") String recommendationServiceHost,
+            @Value("${app.recommendation-service.port}") int recommendationServicePort,
+            @Value("${app.review-service.host}") String reviewServiceHost,
+            @Value("${app.review-service.port}") int reviewServicePort
+    ) {
+        this.restTemplate = restTemplate;
+        this.objectMapper = objectMapper;
+
+        this.productServiceUrl = "http://" + productServiceHost + ":" + productServicePort + "/product/";
+        this.recommendationServiceUrl = "http://" + recommendationServiceHost + ":" + recommendationServicePort + "/recommendation/";
+        this.reviewServiceUrl = "http://" + reviewServiceHost + ":" + reviewServicePort + "/review/";
+    }
+
+    @Override
+    public Product getProduct(int productId) {
+
+        try {
+            String url = productServiceUrl + productId;
+
+            Product product = this.restTemplate.getForObject(url, Product.class);
+            return product;
+        }
+        catch(HttpClientErrorException ex) {
+            switch (ex.getStatusCode()) {
+
+                case NOT_FOUND:
+                    throw new NotFoundException(getErrorMessage(ex));
+
+                case UNPROCESSABLE_ENTITY:
+                    throw new InvalidInputException(getErrorMessage(ex));
+
+                default:
+                    LOG.warn("Got a unexpected HTTP error: {}, will rethrow it", ex.getStatusCode());
+                    LOG.warn("Error Body: {}", ex.getResponseBodyAsString());
+                    throw ex;
+            }
+        }
+    }
+
+    private String getErrorMessage(HttpClientErrorException ex) {
+        try {
+            return objectMapper.readValue(ex.getResponseBodyAsString(), HttpErrorInfo.class).getMessage();
+        } catch (IOException ioex) {
+            return ex.getMessage();
+        }
+    }
+
+    @Override
+    public List<Recommendation> getRecommendations(int productId) {
+        try {
+            String url = recommendationServiceUrl + productId;
+            List<Recommendation> recommendations = this.restTemplate.exchange(
+                            url,
+                            HttpMethod.GET,
+                            null,
+                            new ParameterizedTypeReference<List<Recommendation>>() {
+                            })
+                    .getBody();
+
+            return recommendations;
+        } catch (Exception ex) {
+            LOG.warn("Got an exception while requesting recommendations, return zero recommendations: {}", ex.getMessage());
+            return new ArrayList<>();
+        }
+    }
+
+    @Override
+    public List<Review> getReviews(int productId) {
+        try {
+            String url = reviewServiceUrl + productId;
+            List<Review> reviews = this.restTemplate.exchange(
+                    url,
+                    HttpMethod.GET,
+                    null,
+                    new ParameterizedTypeReference<List<Review>>() {}
+            ).getBody();
+
+            return reviews;
+        }
+        catch (Exception ex) {
+            LOG.warn("Got an exception while requesting recommendations, return zero recommendations: {}", ex.getMessage());
+            return new ArrayList<>();
+        }
+    }
+}
